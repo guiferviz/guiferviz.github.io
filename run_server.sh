df4c628579b967a01acaf658245f6c81791eb9e2
guiferviz <guiferviz@gmail.com>
2020-04-03 12:54:00 +0200

    Adding sudo to the docker command that runs the server.

--- run_server.sh
+++ run_server.sh
@@ -2,7 +2,7 @@
 #jekyll server --watch --drafts --host $IP --port $PORT
 
 export JEKYLL_VERSION=3.8
-docker run --rm \
+sudo docker run --rm \
   --volume="$PWD:/srv/jekyll" \
   -p "4000:4000" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
